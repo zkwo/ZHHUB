8ae8348e0b4d3cfef36f860ce1935271aabc4c9f
zkwo <ko40c980ew632@gmail.com>
2026-02-17 22:40:15 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// Pastikan menggunakan export default langsung tanpa variabel tambahan
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+  }
+})
